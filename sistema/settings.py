a999d7fcd8cdc24f9ad462206ff7137808777ead
Ricardoj626 <ricardoj@gec.inatel.br>
2018-04-23 19:14:29 -0300

    site_id = 1

--- sistema/settings.py
+++ sistema/settings.py
@@ -32,8 +32,8 @@ TIME_ZONE = "America/Sao_Paulo"
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "pt-br"
 
-SITE_ID = int(os.environ.get("SITE_ID", 1))
-
+# SITE_ID = int(os.environ.get("SITE_ID", 1))
+SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
