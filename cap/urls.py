5a935cd221a1ea6460b390d3fb6185f8b5904e4c
Ricardoj626 <ricardoj@gec.inatel.br>
2018-05-28 20:02:39 -0300

    arrumando bug model pm

--- cap/urls.py
+++ cap/urls.py
@@ -5,6 +5,6 @@ from .views import Cap_View, Observacoes_View
 
 urlpatterns = [
     path(r"<int:pk>/obs/", Observacoes_View, name="observacoes"),
-    path(r"^$", Cap_View.as_view(), name="cap"),
+    path(r"", Cap_View.as_view(), name="cap"),
 
 ]
